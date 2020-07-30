8a712bb941288979cddd5c980d33c77d38ed1ec6
GustavoSSLima <gustavosslima@localhost.localdomain>
2020-07-30 20:50:13 -0300

    Category registration page form created

--- src/pages/register/category/index.js
+++ src/pages/register/category/index.js
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
 
 
 function RegisterCategory(){
+  const [categories, setCategories] = useState([]);
+  const [categoryName, setCategoryName] = useState('Food');
+
   return(
     <PageDefault>
-        <h1>Category Register</h1>
+        <h1>Category Register: {categoryName}</h1>
+
+        <form onSubmit={function handleSubmit(eventInfo){
+          eventInfo.preventDefault();
+          setCategories([
+            ...categories,
+            categoryName
+          ]);
+        }}>
+          {/* State */}
+          <label>
+            Category name:
+            <input
+              type="text"
+              value={categoryName}
+              onChange={  function handleFunction(eventInfo) {
+                setCategoryName(eventInfo.target.value);
+              }}
+            />
+          </label>
+
+          <button>
+            Register
+          </button>
+        </form>
+
+        <ul>
+          {categories.map((categories, index) => {
+            return (
+              <li key={`${categories}${index}`}>
+                {categories}
+              </li>
+            )
+          })}
+        </ul>
 
         <Link to="/register/video">
           Back to video register
