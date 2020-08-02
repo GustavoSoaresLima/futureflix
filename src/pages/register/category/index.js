6ce1c97bd56c9238d15594687e9f1535a3c08666
GustavoSSLima <gustavosslima@localhost.localdomain>
2020-08-01 21:18:56 -0300

    Created: FormField component

--- src/pages/register/category/index.js
+++ src/pages/register/category/index.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
-
-function RegisterCategory(){
+function RegisterCategory() {
   const [categories, setCategories] = useState([]);
 
   const initialValues = {
     name: '',
     description: '',
     color: '',
-  }
+  };
   const [values, setValues] = useState(initialValues);
 
   function setValue(key, value) {
     setValues({
       ...values,
       [key]: value, // name: 'value' / description = 'value'
-    })
+    });
   }
 
   function handleChange(eventInfo) {
@@ -25,76 +25,87 @@ function RegisterCategory(){
     setValue(eventInfo.target.getAttribute('name'), value);
   }
 
-  return(
+  return (
     <PageDefault>
-        <h1>Category Register: {values.name}</h1>
+      <h1>Category Register: {values.name}</h1>
 
-        <form onSubmit={function handleSubmit(eventInfo){
+      <form
+        onSubmit={function handleSubmit(eventInfo) {
           eventInfo.preventDefault();
-          setCategories([
-            ...categories,
-            values
-          ]);
+          setCategories([...categories, values]);
 
           setValues(initialValues);
-        }}>
+        }}
+      >
+        <FormField
+          label="Category name"
+          type="text"
+          name="name"
+          value={values.name}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Category name:
+            <input
+              type="text"
+              value={values.name}
+              name="name"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
-          <div>
-            <label>
-              Category name:
-              <input
-                type="text"
-                value={values.name}
-                name="name"
-                onChange={handleChange}
-              />
-            </label>
-          </div>
-          
-          <div>
-            <label>
-              Description:
-              <textarea
-                type="text"
-                value={values.description}
-                name="description"
-                onChange={handleChange}
-              />
-            </label>
-          </div>
+        <FormField
+          label="Description"
+          type="text"
+          name="description"
+          value={values.description}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Description:
+            <textarea
+              type="text"
+              value={values.description}
+              name="description"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
-          <div>
-            <label>
-              Color:
-              <input
-                type="color"
-                value={values.color}
-                name="color"
-                onChange={handleChange}
-              />
-            </label>
-          </div>
+        <FormField
+          label="Color"
+          type="color"
+          name="color"
+          value={values.color}
+          onChange={handleChange}
+        />
+        {/* <div>
+          <label>
+            Color:
+            <input
+              type="color"
+              value={values.color}
+              name="color"
+              onChange={handleChange}
+            />
+          </label>
+        </div> */}
 
-          <button>
-            Register
-          </button>
-        </form>
+        <button>Register</button>
+      </form>
 
-        <ul>
-          {categories.map((categories, index) => {
-            return (
-              <li key={`${categories}${index}`}>
-                {categories.name}
-              </li>
-            )
-          })}
-        </ul>
+      <ul>
+        {categories.map((categories, index) => {
+          return <li key={`${categories}${index}`}>{categories.name}</li>;
+        })}
+      </ul>
 
-        <Link to="/register/video">
-          Back to video register
-        </Link>
+      <Link to="/register/video">Back to video register</Link>
     </PageDefault>
-  )
+  );
 }
 
 export default RegisterCategory;
